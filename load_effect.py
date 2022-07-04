99553f54c1fe45f5a7e937e387488ac99869ac55
Nate Lewis <natedeanlewis@gmail.com>
2022-07-03 23:38:26 -0400

    pi arch update

--- load_effect.py
+++ load_effect.py
@@ -4,7 +4,7 @@ effect_module  = 'effects/' + sys.argv[1]
 sys.path.append(effect_module)
 effect_dir = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], effect_module)))
 sys.path.insert(0, effect_dir)
-print("loading: " + effect_module)
+print("starting " + effect_module)
 import effect
 matrix = Matrix()
 effect.run(matrix, {
--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 # only supports mac/linux and pi
-echo Loading effect...
-
-if [ $(uname -m) = "armv6l" ]; then
-    # pi
+ENV_TYPE=$(uname -m)
+if [ $ENV_TYPE = "armv7l" ]; then
+    echo Running on Raspberry Pi
     sudo python3 load_effect.py "$@"
 else
-    # mac/linux
+    echo Running on $ENV_TYPE
     python3 load_effect.py "$@"
 fi
