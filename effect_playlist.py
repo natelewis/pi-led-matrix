db170c478aebcf326a9c11f558fbf70416fd213c
Nate Lewis <natedeanlewis@gmail.com>
2022-08-06 19:18:20 -0400

    rewrote playist to preload modules instead of loading them on the fly

--- effect_playlist.py
+++ effect_playlist.py
@@ -1,35 +1,51 @@
-import sys
 import os
-import inspect
 import time
 import random
+import inspect
+import importlib
 import multiprocessing
 from src.led_matrix import Matrix, pixel_height, pixel_width, playlist_delay, playlist
 
-# mock target task function
-def run_effect():
-    effect_item = random.choice(playlist)
-    effect_module  = 'effects/' + effect_item['effect']
-    sys.path.append(effect_module)
+def import_effect(name):
     effect_dir = os.path.realpath(
         os.path.abspath(
-            os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], effect_module)
+            os.path.join(
+                os.path.split(inspect.getfile(inspect.currentframe()))[0],
+                'effects/' + name
+            )
         )
     )
-    sys.path.insert(0, effect_dir)
-    print("starting " + effect_module)
-    import effect # pylint: disable=import-outside-toplevel
-    matrix = Matrix()
-    effect.run(matrix, {
-        'pixel_height': pixel_height,
-        'pixel_width': pixel_width,
-        'effect_dir': effect_dir,
-        'argv': effect_item['argv'],
+    return importlib.import_module('effects.' + name +'.effect'), effect_dir
+
+# preload all the effects
+effects = []
+for e_data in playlist:
+    effect_module, e_dir = import_effect(e_data['effect'])
+    effects.append({
+        'name': e_data['effect'],
+        'argv': e_data['argv'],
+        'module': effect_module,
+        'dir': e_dir,
     })
 
+def run_effect(index):
+    effect_obj = effects[index]
+    print("Starting " + effect_obj['name'])
+    # import effect # pylint: disable=import-outside-toplevel
+    matrix = Matrix()
+    effect_obj['module'].run(matrix, {
+        'pixel_height': pixel_height,
+        'pixel_width': pixel_width,
+        'effect_dir': effect_obj['dir'],
+        'argv': effect_obj['argv'],
+    })
+
+
 if __name__ == '__main__':
+    print("Starting playlist: CTRL-c to stop")
     while True:
-        p = multiprocessing.Process(target=run_effect, name="run_effect", args=())
+        effect_index = random.randrange(len(effects))
+        p = multiprocessing.Process(target=run_effect, name="run_effect", args=(effect_index,))
         p.start()
         time.sleep(playlist_delay)
         p.terminate()
