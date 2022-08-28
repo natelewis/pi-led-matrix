72474a489945d480601d9bb84bd9f89695ad5335
Brian Green <briangreen@yahoo.com>
2022-08-28 16:53:08 -0400

    Futbol!  Setup football play and watch them go! (#11)
    * Beta test of warp field effect
    
    * Simple eyes with random movement
    
    * Add multiball effect with random colors/velocity
    Takes number of balls as argument
    
    * Added black hole in center for increased effect
    
    * Sparkler effect, needs some adjustments
    
    * Use new random_color function
    
    * Use common color name to rgb values function
    
    * initial futbol playing field and random actions

---
+++ effects/futbol/effect.py
@@ -0,0 +1,60 @@
+#
+# futbol
+# 28 August 2022
+#
+
+import random
+
+def run(matrix, config):
+    """ futbol """
+
+    x_max = config['pixel_width']
+    y_max = config['pixel_height']
+    play_start = True
+
+    red_pos = [(24, 12), (28, 12), (32, 12), (36, 12), (4, 12), (8, 8), (13, 6), (54, 12), (48, 10), (30, 8), (40, 9)]
+
+    blue_pos = [(24, 18), (28, 18), (32, 18), (36, 18), (30, 23), (26, 23), (55, 23), (44, 20), (32, 26), (8, 22), (12, 24)]
+
+    while True:
+        # Draw a green football field
+        matrix.reset(matrix.color('green'))
+
+        # Draw yard line
+        matrix.line((0, int(y_max/2)), (x_max, int(y_max/2)), matrix.color('white'), 1) 
+
+        # Draw tick marks (sorry the math is a little messy)
+        for tick in range(1, 8):
+            matrix.line((int(x_max/3)-3, int(y_max/8 * tick)), (int(x_max/3), int(y_max/8 * tick)), matrix.color('white'), 1)
+            matrix.line((int(x_max - (x_max/3)), int(y_max/8 * tick)), (int(x_max - (x_max/3) + 3), int(y_max/8 * tick)), matrix.color('white'), 1)
+        
+        # Draw Team Red
+        for x, y in red_pos:
+            matrix.pixel((x, y), matrix.color('red'))
+
+        # Draw Team Blue
+        for x, y in blue_pos:
+            matrix.pixel((x, y), matrix.color('blue'))
+
+        matrix.show()
+
+        # Hut hut hut!
+        if play_start is True:
+            play_start = False
+            matrix.delay(3000)
+
+        # Move red player but do not collide with other players
+        for player in range(len(red_pos)):
+            x, y = red_pos[player]
+            x += random.randint(-1, 1)
+            y += random.randint(-1, 1)
+            if (x, y) not in blue_pos and (x, y) not in red_pos:
+                red_pos[player] = (x, y)
+
+        # Move blue player but do not collide with other players
+        for player in range(len(blue_pos)):
+            x, y = blue_pos[player]
+            x += random.randint(-1, 1)
+            y += random.randint(-1, 1)
+            if (x, y) not in blue_pos and (x, y) not in red_pos:
+                blue_pos[player] = (x, y)
